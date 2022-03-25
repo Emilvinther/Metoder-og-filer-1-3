3ae320acb48fd3bd21be5328c5ca04a38c4e46fb
Emil Lorentzen <emilerher@gmail.com>
2022-03-25 08:57:22 +0100

    Add project files.

---
+++ Metoder og filer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Metoder_og_filer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            WriteToFile();
+            string contents = ReadFile();
+            Console.WriteLine(contents);
+            DeleteFile();
+
+
+
+            Console.ReadKey();
+        }
+
+       public static void WriteToFile()
+       {
+
+            File.WriteAllText(@".\StarWars.txt", "Han skød først");
+
+       }
+
+        public static string ReadFile()
+        {
+           string contents = File.ReadAllText(@".\StarWars.txt");
+
+            return contents;
+        }
+
+        public static void DeleteFile()
+        {
+            File.Delete(@".\StarWars.txt");
+        }
+            
+
+    }
+}
